8754e1c33693e2553c3aca283d921fa450af6bad
sgcharan <sgcharan7@gmail.com>
2022-09-18 19:09:14 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trad-assist-ui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
